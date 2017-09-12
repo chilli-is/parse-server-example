ee08396c28a62efbf366b8aeb31e82daca0889fd
Robini <ian.robinson@chilli-is.co.uk>
2017-09-12 18:12:29 +0100

    Add Project trigger to ensure a title is answered.

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,14 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+
+Parse.Cloud.beforeSave("Project", function(request, response) {
+ 
+  if (!request.object.get("title")) {
+    response.error("A title is required for a project.");
+  } else {
+    response.success();
+  }
+
+});
